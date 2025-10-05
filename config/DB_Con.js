cc7febbd5ec397144b56a19ab00e25bf22912d71
prachi2253 <prachi15goyal@gmail.com>
2025-10-05 15:45:14 +0530

    fixed db connection

--- config/DB_Con.js
+++ config/DB_Con.js
@@ -1,14 +1,20 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-dotenv.config()
+dotenv.config();
+
 const Mongodburl = process.env.URL;
-const dbconnection=async()=>{
-try{
-    await mongoose.connect(Mongodburl);
-    console.log("Data Base Connected Successfully");
-}catch(e){
-    console.log("Error Occurred At Connecting Database:", e.message);
-    console.log("Make sure MongoDB is running on:", Mongodburl);
-}
-}
-module.exports= dbconnection
+
+const dbconnection = async () => {
+  try {
+    await mongoose.connect(Mongodburl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database Connected Successfully");
+  } catch (e) {
+    console.error("Error Occurred At Connecting Database:", e.message);
+    console.error("Make sure MongoDB is running on:", Mongodburl);
+  }
+};
+
+module.exports = dbconnection;
